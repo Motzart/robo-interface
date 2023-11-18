8b451188eecd095324e60c8f4da4c1e0f5a6e188
Dmitriy Bublienko <d.bublienko@gmail.com>
2023-11-18 23:10:17 +0200

    Put shared and pseudo element styles in separate file for NavItem

--- src/components/Layouts/Nav/NavItem.tsx
+++ src/components/Layouts/Nav/NavItem.tsx
@@ -3,21 +3,17 @@ import Image from 'next/image';
 import NextLink from 'next/link';
 import { usePathname } from 'next/navigation';
 import { NavItem as TypeNavItem } from './data';
+import {
+  disabledPseudoStyles,
+  activePseudoStyles,
+  sharedStyles,
+} from './styles';
 
 const NavItem = ({ data }: { data: TypeNavItem }) => {
   const pathname = usePathname();
   const { icon, text, href, disabled } = data;
 
   const isActive = pathname === href;
-
-  const disabledPseudoStyles =
-    "after:content-['Soon'] after:absolute after:bottom-[-12px] after:bg-[#FF564E] after:rounded-[24px] after:px-[4px] after:text-white after:text-[11px] after:text-center after:font-medium";
-
-  const activePseudoStyles =
-    "after:content-[''] after:absolute after:right-0 after:top-0 after:w-[2px] after:h-full after:bg-[#4CADEA]";
-
-  const sharedStyles =
-    'flex flex-col items-center justify-center max-w-[76px] p-[7px] text-[12px] font-bold text-[#4C4C4C]';
 
   const sharedComponent = (
     <>
---
+++ src/components/Layouts/Nav/styles.tsx
@@ -0,0 +1,8 @@
+export const disabledPseudoStyles =
+  "after:content-['Soon'] after:absolute after:bottom-[-12px] after:bg-[#FF564E] after:rounded-[24px] after:px-[4px] after:text-white after:text-[11px] after:text-center after:font-medium";
+
+export const activePseudoStyles =
+  "after:content-[''] after:absolute after:right-0 after:top-0 after:w-[2px] after:h-full after:bg-[#4CADEA]";
+
+export const sharedStyles =
+  'flex flex-col items-center justify-center max-w-[76px] p-[7px] text-[12px] font-bold text-[#4C4C4C]';
